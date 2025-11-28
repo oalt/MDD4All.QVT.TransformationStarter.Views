2c8f8acdc3de075cf4085f5b65f49f0989bb58ce
oalt <oa@lacheweg21.de>
2025-11-28 17:28:51 +0100

    Adding object deserialization view

---
+++ src/MDD4All.QVT.TransformationStarter.Views/ObjectDeserializationView.razor.cs
@@ -0,0 +1,28 @@
+using MDD4All.QVT.TransformationStarter.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace MDD4All.QVT.TransformationStarter.Views
+{
+    public partial class ObjectDeserializationView
+    {
+        [Parameter]
+        public ObjectDeserializationViewModel DataContext { get; set; }
+
+        private void OnFormatSelection(ChangeEventArgs changeEventArgs, string format)
+        {
+
+            DataContext.Format = changeEventArgs.Value.ToString();
+
+        }
+
+        private void OnSelectFileToOpen()
+        {
+            SynchronizationContext.Current?.Post((_) =>
+            {
+                DataContext.SelectFileToLoadCommand.Execute("Select data file...");
+                StateHasChanged();
+            }, null);
+
+        }
+    }
+}
